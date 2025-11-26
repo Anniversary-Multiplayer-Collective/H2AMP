c408198f2d340be66717efcdfeee3240cab3ed5c
Zekken <brodycas3@gmail.com>
2025-11-25 23:28:01 -0500

    Add build timer

--- build.bat
+++ build.bat
@@ -1,4 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem start time
+for /f "tokens=1-3 delims=:." %%a in ("%TIME%") do (
+    set /a start=%%a*3600 + %%b*60 + %%c
+)
+
 cd Release_x64_Dev
 type ..\Halo_2.cmds|tagtool.exe
+
+rem end time
+for /f "tokens=1-3 delims=:." %%a in ("%TIME%") do (
+    set /a end=%%a*3600 + %%b*60 + %%c
+)
+
+set /a elapsed=end-start
+set /a minutes=elapsed/60
+set /a seconds=elapsed%%60
+
+if %seconds% lss 10 set seconds=0%seconds%
+
+echo Time taken: %minutes%:%seconds%
 
 pause
